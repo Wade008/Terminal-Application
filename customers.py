687a715e660673445c35afe1277f0297a3ccadf4
Wade Doolan <wadedoolan@hotmail.com>
2022-07-09 20:59:29 +1000

    add add_customer method to customers class

--- customers.py
+++ customers.py
@@ -3,12 +3,13 @@ from customer import Customer
 class Customers:
     
     def __init__(self, customer_list):
-        self.custumer_list = customer_list
+        self.customer_list = customer_list
 
     def show_customers(self):
         print("List of customers in the system")
         for customer in self.customer_list:
             customer.show_customer()
 
-
+    def add_customer(self, fname, lname, phone, email):
+        self.customer_list.append(Customer(fname, lname, phone, email))
 
--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-def main_menu():
-    pass
+from curses import longname
+import email
+from seed import customer_seed
+# Maybe import math
+
+
+customers = customer_seed()
+
+customers.show_customers()
+
+
+def add_customer():
+    fname = input("Customer first name: ")
+    lname = input("Customer last name: ")
+    phone = input("Customer Phone number: ")
+    email = input("Customer Email address: ")
+
+    customers.add_customer(fname, lname, phone, email)
+
+    print(f"{fname} {lname} has been added to the system")
+
+
+
+add_customer()
+
+customers.show_customers()
+
+
+
+
+
 
