00bf7531da7e2727e3b37c4fc87b6b4f521b4f9f
Wade Doolan <wadedoolan@hotmail.com>
2022-07-09 19:41:05 +1000

    create customer class

---
+++ customer.py
@@ -0,0 +1,16 @@
+
+class Customer:
+    # A class to instantiate a customer object
+    def __init__(self, fname, lname, phone, email):
+        self.firstname = fname
+        self.lastname = lname
+        self.phone = phone
+        self.email = email
+
+    def show_customer(self):
+        print(f"Customer: {self.firstname} {self.lastname}\nPhone: {self.phone}\nEmail: {self.email}")
+
+
+# customer1 = Customer("Wade", "Doolan", "0448175351", "wadedoolan@hotmail.com")
+
+# customer1.show_customer()
