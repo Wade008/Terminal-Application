a2ff113330e4d507a25477cc2f7ec048fce6aead
Wade Doolan <wadedoolan@hotmail.com>
2022-07-10 17:51:33 +1000

    add delete_customer method

--- customers.py
+++ customers.py
@@ -1,7 +1,8 @@
 from customer import Customer
 
+
 class Customers:
-    
+
     def __init__(self, customer_list):
         self.customer_list = customer_list
 
@@ -14,16 +15,21 @@ class Customers:
         self.customer_list.append(Customer(fname, lname, phone, email))
 
     def find_customer(self, search):
-        #iterate over customer list searching for customer
-        #only look using phone or email as these are unique ids
+        # iterate over customer list searching for customer
+        # only look using phone or email as these are unique ids
 
         for customer in self.customer_list:
             if customer.phone == search or customer.email == search:
                 return customer
-            
 
-        
+    def delete_customer(self, customer):
+        # using the find method above pass in the current customer
+        self.customer_list.remove(customer)
 
+        return print(f"{customer.firstname} {customer.lastname} was successfully removed from the system.")
+
+    def update_customer(self, customer):
+        pass
 
 
 
@@ -38,5 +44,3 @@ class Customers:
     #     for customer in self.customer_list:
 
     #         if customer.phone == phone or customer.email == email:
-
-
--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 
+from  os import system
+from datetime import datetime
 from seed import customer_seed
 # Maybe import math
 
-
+# create a new customers object and seed it
 customers = customer_seed()
 
-customers.show_customers()
+# customers.show_customers()
+
+# Welcome message with current date
+def _welcome():
+    date_time = datetime.now()
+    today_name = date_time.strftime("%A")
+    today = date_time.strftime("%d %B, %Y")
+    if date_time.hour < 12:
+        greeting = "Good morning"
+    if date_time.hour < 17:
+        greeting = "Good afternoon"
+    else:
+        greeting = "Good evening"
 
 
-def add_customer():
+    print(f"{greeting}! Welcome to the store credit application.")
+    print(f"It's {today_name} {today}\n")
+  
+
+# Main menu for retail staff
+def _main_menu():
+    print("Main menu. Select from the options below:\n")
+    print("a. Update or view a customer's credit")
+    print("b. Update or view a customer's details")
+    print("c. Add a new customer")
+    print("d. Remove a customer")
+    print("e. View all store customers")
+    print("f. View total outstanding credit for the store")
+    print("q. Exit application\n")
+
+    selection = input("What would you like to do? (Enter a-f or q to exit): ").strip()
+    return selection
+
+def _add_customer():
+    print("Enter customer details below")
     fname = input("Customer first name: ")
     lname = input("Customer last name: ")
     phone = input("Customer Phone number: ")
@@ -19,26 +52,61 @@ def add_customer():
     print(f"{fname} {lname} has been added to the system")
 
 
-def find_customer():
+def _find_customer():
 
     lookup = input(
-        "Search for a customer by entering either their email or phone: ")
+        "Search for a customer by entering either their email or phone: ").strip()
 
     customer = customers.find_customer(lookup)
 
     try: 
-        print(f"\nCustomer details below:\n Name: {customer.firstname} {customer.lastname}\n Phone: {customer.phone}\n Email: {customer.email}")
+        print(f"\nCustomer details below:\n Name: {customer.firstname} {customer.lastname}\n Phone: {customer.phone}\n Email: {customer.email}\n")
+        print("1. Edit customer")
+        print("2. Delete customer\n")
+        response = input("What would you like to do? (Enter 1-2 or any other key to return to the main menu).").strip()
+        if response == "1":
+            pass
+        elif response == "2":
+            customers.delete_customer(customer)
+
+
     except AttributeError:
-        again = input("No customer found. Try again? (type y to search again pr any other key to exit) ")
+        again = input("No customer found. Try again? (type y to search again or press any other key to exit) ")
         if again == "y":
-            find_customer()
+            _find_customer()
         else:
             print("Exiting customer search... Bye!")
 
 
+def _show_all_customers():
+    customers.show_customers()
 
-find_customer()
+
+# while loop to start main menu
+
+selection = ""
+
+while selection != "q":
+
+    system("clear")
+    _welcome()
+
+    selection = _main_menu()
+
+    if selection == "b" or selection == "d":
+        _find_customer()
+    elif selection == "c":
+        _add_customer()
+    elif selection == "e":
+        _show_all_customers()
+    elif selection == "q":
+        continue
+    else:
+         print("That's not an option. Try again.")
+    
+    input("Press any key to return to the main menu")
+    system("clear")
+
+print("Exiting application... Bye!")
 
 
-# add_customer()
-# customers.show_customers()
