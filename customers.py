770e2b2d3e13ec23508e9f9b4e2b5405c1848f46
Wade Doolan <wadedoolan@hotmail.com>
2022-07-10 12:37:25 +1000

    add find_customer method

--- customers.py
+++ customers.py
@@ -13,3 +13,30 @@ class Customers:
     def add_customer(self, fname, lname, phone, email):
         self.customer_list.append(Customer(fname, lname, phone, email))
 
+    def find_customer(self, search):
+        #iterate over customer list searching for customer
+        #only look using phone or email as these are unique ids
+
+        for customer in self.customer_list:
+            if customer.phone == search or customer.email == search:
+                return customer
+            
+
+        
+
+
+
+
+
+
+
+    # def remove_customer(self, phone, email):
+
+    #     #iterate over customer list searching for customer
+    #     #only look using phone or email as these are unique ids
+
+    #     for customer in self.customer_list:
+
+    #         if customer.phone == phone or customer.email == email:
+
+
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from curses import longname
-import email
+
 from seed import customer_seed
 # Maybe import math
 
@@ -20,13 +19,26 @@ def add_customer():
     print(f"{fname} {lname} has been added to the system")
 
 
+def find_customer():
 
-add_customer()
+    lookup = input(
+        "Search for a customer by entering either their email or phone: ")
 
-customers.show_customers()
+    customer = customers.find_customer(lookup)
+
+    try: 
+        print(f"\nCustomer details below:\n Name: {customer.firstname} {customer.lastname}\n Phone: {customer.phone}\n Email: {customer.email}")
+    except AttributeError:
+        again = input("No customer found. Try again? (type y to search again pr any other key to exit) ")
+        if again == "y":
+            find_customer()
+        else:
+            print("Exiting customer search... Bye!")
 
 
 
+find_customer()
 
 
-
+# add_customer()
+# customers.show_customers()
